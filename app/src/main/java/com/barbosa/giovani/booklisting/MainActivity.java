b8516cebfa399c2dd77216607d415da91f14d1ae
giovani <giovanibarbosa@gmail.com>
2018-02-17 17:15:10 -0300

    List view, adapters and basic layouts

--- app/src/main/java/com/barbosa/giovani/booklisting/MainActivity.java
+++ app/src/main/java/com/barbosa/giovani/booklisting/MainActivity.java
@@ -2,37 +2,64 @@ package com.barbosa.giovani.booklisting;
 
 import android.app.LoaderManager;
 import android.content.Loader;
-import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ListView;
+import android.widget.TextView;
 
+import com.barbosa.giovani.booklisting.adapters.BooksAdapter;
 import com.barbosa.giovani.booklisting.loaders.BooksLoader;
 import com.barbosa.giovani.booklisting.model.Book;
-import com.barbosa.giovani.booklisting.utils.Utils;
 
-import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements LoaderManager
         .LoaderCallbacks<List<Book>> {
-    private static final String BOOKS_REQUEST = "https://www.googleapis.com/books/v1/volumes?q=flowers";
+    /** Book volumes url. */
+    private static final String BOOK_VOLUME_URL = "https://www.googleapis.com/books/v1/volumes?q=";
 
+    /** String representing the book volumes query. */
+    private String mQuery;
+
+    /** StringBuilder for the request. */
+    private StringBuilder mRequestBuilder;
+
+    /** Log tag. */
     private static final String TAG = MainActivity.class.getName();
 
+    /** Id for the book loader. */
     private static final int BOOK_LOADER_ID = 1;
+
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        getLoaderManager().initLoader(BOOK_LOADER_ID, null, this);
+        List<Book> bookList = new ArrayList<>();
+
+        BooksAdapter adapter = new BooksAdapter(this, bookList);
+
+        mListView = findViewById(R.id.book_list_view);
+        mListView.setAdapter(adapter);
+
+        TextView emptyTextView = findViewById(R.id.empty_list_view);
+        mListView.setEmptyView(emptyTextView);
+
+//        mRequestBuilder = new StringBuilder(BOOK_VOLUME_URL);
+
+        if (mQuery != null && !mQuery.isEmpty()) {
+            getLoaderManager().initLoader(BOOK_LOADER_ID, null, this);
+        }
     }
 
     @Override
     public Loader<List<Book>> onCreateLoader(int id, Bundle args) {
-        return new BooksLoader(this, BOOKS_REQUEST);
+        return null;
+//        return new BooksLoader(this, BOOKS_REQUEST);
     }
 
     @Override
---
+++ app/src/main/java/com/barbosa/giovani/booklisting/adapters/BooksAdapter.java
@@ -0,0 +1,74 @@
+package com.barbosa.giovani.booklisting.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.barbosa.giovani.booklisting.R;
+import com.barbosa.giovani.booklisting.model.Book;
+
+import java.util.List;
+
+/**
+ * Adapter for book list view.
+ */
+public class BooksAdapter extends BaseAdapter {
+    /** The context */
+    private Context mContext;
+
+    /** The book list. */
+    private List<Book> mBooks;
+
+    public BooksAdapter(Context context, List<Book> books) {
+        this.mContext = context;
+        mBooks = books;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.book_list_item, parent,
+                    false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        Book currentBook = (Book) getItem(position);
+        viewHolder.titleTextView.setText(currentBook.getTitle());
+//        viewHolder.authorsLayoututhors.setText(currentBook.getItemDescription());
+
+        return convertView;
+    }
+
+    private class ViewHolder {
+        TextView titleTextView;
+        View authorsLayout;
+
+        private ViewHolder(View view) {
+            titleTextView = view.findViewById(R.id.title_text_view);
+            authorsLayout = view.findViewById(R.id.authors_view);
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return mBooks.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return mBooks.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+}
